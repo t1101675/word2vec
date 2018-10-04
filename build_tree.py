1a2d8b4420d33661292ef936eed95ce7f3c7a3c8
顾煜贤 <2017011421@secoder.net>
2018-10-04 16:20:33 +0800

    finish huffman tree

--- build_tree.py
+++ build_tree.py
@@ -12,6 +12,8 @@ class HuffmanTree:
     def __init__(self, wordList, vecLength):
         self.nodeList = [HuffmanTreeNode(word["value"], word["posibility"]) for word in wordList]
         self.vecLength = vecLength
+        self.root = None
+        self.buildTree()
 
     def buildTree(self):
         while (len(nodeList) > 1):
@@ -34,6 +36,14 @@ class HuffmanTree:
             else:
                 raise RuntimeError("min1 = min2")
             nodeList.append(newNode)
-    def merge(node1, node2):
-        newNode = HuffmanTreeNode(np.zeros)
-        pass
+        self.root = nodeList[0]
+
+    def merge(self, node1, node2):
+        newNode = HuffmanTreeNode(np.zeros(self.vecLength), node1.posibility + node2.posibility)
+        if node1.posibility > node2.posibility:
+            newNode.left = node1
+            newNode.right = node2
+        else:
+            newNode.right = node1
+            newNode.left = node2
+        return newNode
